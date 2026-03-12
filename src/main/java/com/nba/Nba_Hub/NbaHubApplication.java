5021f159b0d03cd598f2660af117ed2f56d6534a
Elijah Waldron <Elijahwaldrons@gmail.com>
2026-03-12 15:55:54 -0400

    Init for up for Nba Rest API

---
+++ src/main/java/com/nba/Nba_Hub/NbaHubApplication.java
@@ -0,0 +1,13 @@
+package com.nba.Nba_Hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nba/Nba_Hub/NbaHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nba.Nba_Hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
